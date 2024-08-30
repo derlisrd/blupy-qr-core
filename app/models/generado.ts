5afe61b16a7b5ab1d9ae4a41524df6532b536ed0
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-08-30 12:15:20 -0400

    solo hasta 10 digitos

--- app/models/generado.ts
+++ app/models/generado.ts
@@ -16,9 +16,7 @@ export default class Generado extends BaseModel {
 
   @beforeCreate()
   static async setID(generado: Generado) {
-    const timestamp = Date.now().toString()
-    const randomNumber = Math.floor(Math.random() * 1000).toString().padStart(3, '0')
-    generado.codigo = `${timestamp}${randomNumber}`
+    generado.codigo = await Generado.generateUniqueCode()
   }
 
   @column({ isPrimary: true })
@@ -81,4 +79,23 @@ export default class Generado extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  static async generateUniqueCode(): Promise<string> {
+    let isUnique = false
+    let codigo = ''
+
+    while (!isUnique) {
+      // Genera un número aleatorio entre 1 y 10 dígitos
+      const length = Math.floor(Math.random() * 10) + 1
+      codigo = Math.floor(Math.random() * Math.pow(10, length)).toString()
+
+      // Verifica si el código ya existe en la base de datos
+      const existing = await Generado.findBy('codigo', codigo)
+      if (existing == null) {
+        isUnique = true
+      }
+    }
+
+    return codigo
+  }
 }
