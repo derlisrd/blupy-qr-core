53f4502e6cef6a4961d2b62ac830cb076827e22e
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-09-18 16:59:12 -0400

    adicional

--- app/controllers/generados_clientes_controller.ts
+++ app/controllers/generados_clientes_controller.ts
@@ -70,6 +70,7 @@ export default class GeneradosClientesController {
       generado.status = 1
       generado.numero_movimiento = TcMovNro
       generado.numero_cuenta = req.numero_cuenta
+      generado.adicional = req.adicional
       await generado.save()
 
       auditoria.telefono = req.telefono
