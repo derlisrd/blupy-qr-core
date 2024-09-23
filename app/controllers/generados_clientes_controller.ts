40dd1a9fe81c9cb8271222cabd9836d4c1dee3a0
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-09-23 16:49:13 -0400

    loguer

--- app/controllers/generados_clientes_controller.ts
+++ app/controllers/generados_clientes_controller.ts
@@ -96,8 +96,6 @@ export default class GeneradosClientesController {
       return response.json({ success: true, message: 'Autorizado', results })
     } catch (error) {
       console.log(error)
-      logger.error(error)
-      logger.error(String(error))
       logger.error(JSON.stringify(error))
       console.log(JSON.stringify(error))
       // const message = error.messages[0].message ?? 'Error de servidor'
