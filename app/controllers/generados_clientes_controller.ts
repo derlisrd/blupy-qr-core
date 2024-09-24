c86fdc6aa9750460da1cf2c5713f15359e5e5fc9
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-09-24 17:35:07 -0400

    verificar error

--- app/controllers/generados_clientes_controller.ts
+++ app/controllers/generados_clientes_controller.ts
@@ -96,10 +96,10 @@ export default class GeneradosClientesController {
       // console.log('result', results)
       return response.json({ success: true, message: 'Autorizado', results })
     } catch (error) {
-
+      console.log(error)
       logger.error(JSON.stringify(error))
       // const message = error.messages[0].message ?? 'Error de servidor'
-      return response.status(500).json({ success: false, message: 'Error de servidor' })
+      return response.status(500).json({ success: false, message: 'Error de servidor. BQ501' })
     }
   }
 
