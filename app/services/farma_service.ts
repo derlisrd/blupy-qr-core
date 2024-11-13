a912ed4b173319aa01b5afb3cb1f4ea571ba93c7
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-11-13 12:04:02 -0300

    confirmar

--- app/services/farma_service.ts
+++ app/services/farma_service.ts
@@ -3,11 +3,12 @@ import axios from 'axios'
 import env from '#start/env'
 
 export const ConfirmarPago = async (data: any) => {
+  console.log({ username: env.get('USERNAME_FARMA') , password: env.get('PASSWORD_FARMA') })
   try {
     const res = await axios.post(`${env.get('APIURL_FARMA')}/confirmar`, data, {
       headers: {
         'username': env.get('USERNAME_FARMA'),
-        'password': env.get('PASSWORD_FARMA'),
+        'password': 'cK(<8W5+=$4_!9ZA,5Eliih^M?q&A~%f_]',
         'Content-Type': 'application/json'
       }
     })
