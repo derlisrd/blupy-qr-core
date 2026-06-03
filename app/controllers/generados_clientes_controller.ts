5cdf9760ffe8a55e1822e98142890b9671093c3a
derlisrd <derlisruizdiaz@hotmail.com>
2026-06-03 11:13:36 -0300

    fix comfirmar pago

--- app/controllers/generados_clientes_controller.ts
+++ app/controllers/generados_clientes_controller.ts
@@ -1,6 +1,6 @@
 import Generado from '#models/generado'
 import GeneradoAuditoria from '#models/generados_auditoria'
-//import { ConfirmarPago } from '#services/farma_service'
+import { ConfirmarPago } from '#services/farma_service'
 import { /* ListarTarjetasPorDoc, */ RegistrarTransaccion } from '#services/infinita_service'
 import logger from '@adonisjs/core/services/logger'
 import { autorizarQRValidator } from '#validators/generar'
@@ -119,7 +119,7 @@ export default class GeneradosClientesController {
       const respuesta = { success: true, message: 'Autorizado', results }
       // confirmar pago con farma
       if (generado.web) {
-        //await ConfirmarPago(respuesta)
+        await ConfirmarPago(respuesta)
       }
 
       return response.json(respuesta)
